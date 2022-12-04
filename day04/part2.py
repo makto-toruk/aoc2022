7d009776db23543ca769df01e691bee47f954604
makto-toruk <manasijv93@gmail.com>
2022-12-03 21:26:44 -0800

    day04 solutions

---
+++ day04/part2.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import argparse
+import os
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(input: str) -> int:
+
+    pairs = [i.split(",") for i in input.splitlines()]
+
+    n = 0
+    for l, r in pairs:
+        lmin, lmax = [int(i) for i in l.split("-")]
+        rmin, rmax = [int(i) for i in r.split("-")]
+
+        if (lmin <= rmin <= lmax) or (rmin <= lmin <= rmax):
+            n += 1
+
+    return n
+
+
+INPUT_S = """\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"""
+EXPECTED = 4
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int | None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.input) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
